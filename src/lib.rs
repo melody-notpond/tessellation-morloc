b1f11a584965ba4520cac0f0d18b3a74a9dae5cd
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-31 09:34:40 -0400

    Tried to implement tile_tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 type Vertex2 = (f32, f32);
 
 pub type Polygon = Vec<Vertex2>;
@@ -5,11 +8,11 @@ pub type Polygon = Vec<Vertex2>;
 // eventually add additional forms of tesselation, for now just have regular
 // tiling, with values such as "3.4.6.4"
 pub enum Tesselation {
-    Regular(Vec<i64>)
+    Regular(Vec<u32>),
 }
 
 // an infinite tiling with but scaled and anchored at an origin (offsets from
-// the original vertex) 
+// the original vertex)
 pub struct ScaledTesselation {
     angle: f32,
     scale: f32,
@@ -19,41 +22,118 @@ pub struct ScaledTesselation {
 
 struct RoseTree {
     node: Vertex2,
-    children: Vec<RoseTree>
+    angle: f32,
+    children: Vec<Rc<RefCell<RoseTree>>>,
 }
 
 pub struct Mesh {
     pub polys: Vec<Polygon>,
     pub width: u32,
-    pub height: u32
+    pub height: u32,
 }
 
-// stub
-fn tile_tree(st: ScaledTesselation, mut depth: i64) -> RoseTree {
-    let mut tree = RoseTree {
-        node: st.offset,
-        children: vec![]
-    };
-    let mut queue: Vec<()> = vec![];
+fn rotate_around(p: (f32, f32), pivot: (f32, f32), angle: f32) -> (f32, f32) {
+    // (x+yi)(cost+isint)=xcost-ysint+i(xsint+ycost)
+    let shifted = (p.0 - pivot.0, p.1 - pivot.1);
+    let (cos, sin) = (angle.cos(), angle.sin());
+    let rotated = (
+        shifted.0 * cos - shifted.1 * sin,
+        shifted.0 * sin + shifted.1 * cos,
+    );
+    (rotated.0 + pivot.0, rotated.1 + pivot.1)
+}
 
-    while depth > 0 {
-        depth -= 1;
+fn soft_contains(vec: &Vec<(f32, f32)>, v: (f32, f32), epsilon: f32) -> bool {
+    for a in vec {
+        let diff = (v.0 - a.0, v.1 - a.1);
+        if -epsilon < diff.0 && diff.0 < -epsilon && -epsilon < diff.1 && diff.1 < -epsilon {
+            return true;
+        }
     }
 
-    tree
+    false
 }
 
 // stub
-fn boxed_tile_tree(st: ScaledTesselation, width: f64, height: f64) -> RoseTree {
+fn tile_tree(st: &ScaledTesselation, mut depth: u32) -> Rc<RefCell<RoseTree>> {
+    use std::collections::VecDeque;
+
+    let mut seen = Vec::new();
+    let mut queue = VecDeque::new();
+    let mut queue2 = VecDeque::new();
+
+    let root = RoseTree {
+        node: (0.0, 0.0),
+        angle: 0.0,
+        children: Vec::new(),
+    };
+
+    let root = Rc::new(RefCell::new(root));
+    if depth == 0 {
+        return root;
+    }
+
+    queue.push_back(root.clone());
+
+    while depth > 0 {
+        // Get next node
+        match queue.pop_front() {
+            Some(node) => {
+                if soft_contains(&seen, node.borrow().node, 0.000001) {
+                    println!("found {:?} again", node.borrow().node);
+                    continue;
+                }
+
+                // Create child nodes
+                match &st.spec {
+                    Tesselation::Regular(tes) => {
+                        let pivot = node.borrow().node;
+                        let p = (pivot.0 + 1.0, pivot.1);
+                        let a = node.borrow().angle;
+                        for i in tes {
+                            let angle: f32 = 180.0 * (*i as f32 - 2.0) + a;
+                            let point = rotate_around(p, pivot, angle);
+                            let child = Rc::new(RefCell::new(RoseTree {
+                                node: point,
+                                angle,
+                                children: vec![]
+                            }));
+                            node.borrow_mut().children.push(child.clone());
+                            queue2.push_back(child);
+                        }
+                    }
+                }
+
+                // Push new node
+                seen.push(node.borrow().node);
+            }
+
+            // No node = next depth
+            None => {
+                use std::mem::swap;
+                swap(&mut queue, &mut queue2);
+                queue2.clear();
+                depth -= 1;
+            }
+        }
+    }
+
+    // Root is available by this time
+    root
+}
+
+// stub
+fn boxed_tile_tree(st: &ScaledTesselation, width: f64, height: f64) -> RoseTree {
     RoseTree {
         node: st.offset,
-        children: vec![]
+        angle: 0.0,
+        children: vec![],
     }
 }
 
 // stub
 pub fn render(mesh: &Mesh, filename: &str) {
-    use draw::{Canvas, Drawing, shape::LineBuilder, Style, Color, SvgRenderer};
+    use draw::{shape::LineBuilder, Canvas, Color, Drawing, Style, SvgRenderer};
 
     let mut canvas = Canvas::new(mesh.width, mesh.height);
 
@@ -86,7 +166,7 @@ lookupTile :: Point -> Mesh a -> (Polygon, a)
 -- alter a value stored in a tile that contains a point
 alterTile :: Point -> (a -> a) -> Mesh a -> Mesh a
 
--- alter all values for all tiles that are similar polygons to the one that contains the point 
+-- alter all values for all tiles that are similar polygons to the one that contains the point
 alterSimilar :: Point -> (a -> a) -> Mesh a -> Mesh a
 
 -- rotate the entire mesh
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use tesselation_morloc::*;
 
 fn main() {
-    render(&Mesh {
-        polys: vec![vec![(10.0, 10.0), (10.0, 90.0), (90.0, 90.0), (90.0, 10.0)]],
-        width: 100,
-        height: 100,
-    }, "poly.svg")
+    let tes = Tesselation::Regular(vec![3, 4, 6, 4]);
+
+    render(
+        &Mesh {
+            polys: vec![vec![(10.0, 10.0), (10.0, 90.0), (90.0, 90.0), (90.0, 10.0)]],
+            width: 100,
+            height: 100,
+        },
+        "poly.svg",
+    )
 }
